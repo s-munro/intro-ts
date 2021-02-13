97f393464e89f4c728b3e0ae7ba0e4bb8d9c6e01
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-13 16:49:06 -0500

    chore: added map and interface for map

---
+++ src/CustomMap.ts
@@ -0,0 +1,33 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(target: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: target.location.lat,
+        lng: target.location.lng,
+      },
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hi there");
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
+customMap.addMarker(user);
+customMap.addMarker(company);
